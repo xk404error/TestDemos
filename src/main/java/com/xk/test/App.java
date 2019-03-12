99d0c2df200b5ddb418a1ee412248fed51b39302
xk404error <xikai404error@outlook.com>
2019-03-12 22:13:34 +0800

    第一次

---
+++ src/main/java/com/xk/test/App.java
@@ -0,0 +1,13 @@
+package com.xk.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
